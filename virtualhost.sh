00098aabc3c139de9ffd58ebd6550ed9ac38f856
Bjorn Wijers <burobjorn@burobjorn.nl>
2012-06-08 14:23:51 +0200

    Now more compatible with Ubuntu 12.04 & new features
    - Fixes: Made the script more compatible with Ubuntu 12.04 LTS
    (Precise Pangolin) using the ports.conf file, pinky instead of finger,
    checks apache2.conf for include statement of the Ubuntu/Debian standard
    'sites-enabled' configuration directory and add it if it's not found.
    
    - New: Added command line parameter '--version' to check for the version
    of the script.
    
    - New: Added CREATE_INDEX variable. Set to yes to explicitly create an
    index.html file if none was found. By default set to no. So no index.html
    will be created from now on.
    
    - New: Added ERROR_LOG variable. Set to /var/log/apache2, but can be easily
    easily changed to set the VirtualHost's errorlog. Uses the following format:
    $VIRTUALHOST-error.log

--- virtualhost.sh
+++ virtualhost.sh
@@ -1,65 +1,92 @@
 #!/bin/sh
-#================================================================================
-# virtualhost.sh
+
+# Virtualhost.sh
 #
-# A fancy little script to setup a new virtualhost in Ubuntu based upon the
-# excellent virtualhost (V1.04) script by Patrick Gibson <patrick@patrickg.com> for OS X.
+# Is a nice little script to setup a new virtualhost in Ubuntu based upon the
+# excellent virtualhost script by Patrick Gibson <patrick@patrickg.com> for OS X.
 #
-# This script has been tested on Ubuntu 7.10 (Gutsy Gibbon) with Apache2(!) and
-# probably works on Debian as well, but this has not been tested (yet). If you use
-# this script on other Linux distributions and can confirm it to work I would like to hear
-# from you. Just send an email to Bjorn Wijers <burobjorn@burobjorn.nl> with more info
+# This script has been updated to work on Ubuntu 12.04 (Precise Pangolin) with 
+# Apache2 (version 2.2.22) and probably works on Debian as well, but this has
+# not been tested (yet). Feel free to test it on other Linux distributions.
+# If you encounter any issues feel free to send bugreports & patches 
+# Just send an email to Bjorn Wijers <burobjorn@burobjorn.nl>.
+
+# = CHANGELOG =
+# 
+# 12.04-1
+#    - Fixes: Made the script more compatible with Ubuntu 12.04 LTS
+#    (Precise Pangolin) using the ports.conf file, pinky instead of finger,
+#    checks apache2.conf for include statement of the Ubuntu/Debian standard
+#    'sites-enabled' configuration directory and add it if it's not found.
 #
-# USAGE:
+#    - New: Added command line parameter '--version' to check for the version
+#    of the script.
 #
-# CREATE A VIRTUAL HOST:
+#    - New: Added CREATE_INDEX variable. Set to yes to explicitly create an
+#    index.html file if none was found. By default set to no. So no index.html
+#    will be created from now on.
+#
+#    - New: Added ERROR_LOG variable. Set to /var/log/apache2, but can be easily 
+#    easily changed to set the VirtualHost's errorlog. Uses the following format:
+#    $VIRTUALHOST-error.log
+#
+# = USAGE =
+#
+# 1. Create a VirtualHost:
 # sudo ./virtualhost <name>
-# where <name> is the one-word name you'd like to use. (e.g. mysite)
+# where <name> is the one-word name you'd like to use. (e.g. mysite.dev)
 #
 # Note that if "virtualhost.sh" is not in your PATH, you will have to write
 # out the full path to where you've placed: eg. /usr/bin/virtualhost.sh <name>
 #
-# REMOVE A VIRTUAL HOST:
+# 2. Remove a VirtualHost:
 # sudo ./virtualhost --delete <site>
 #
 # where <site> is the site name you used when you first created the host.
-#
-#
-#======= SCRIPT VARIABLES ========
+
+
+# == SCRIPT VARIABLES ==
 #
 # If you are using this script on a production machine with a static IP address,
-# and you wish to setup a "live" virtualhost, you can change the following IP
+# and you wish to setup a "live" virtualhost, you can change the following '*'
 # address to the IP address of your machine.
+ IP_ADDRESS="127.0.0.1"
 #
-IP_ADDRESS="127.0.0.1"
-#
-# By default, this script places files in /home/[you]/Sites. If you would like
-# to change this, like to how Apache on Ubuntu does things by default, uncomment the
-# following line:
+# By default, this script places files in /home/[username]/Sites. If you would like
+# to change this uncomment the following line:
 #
 #DOC_ROOT_PREFIX="/var/www"
 #
 # Configure the apache-related paths if these defaults do not work for you.
 #
-APACHE_CONFIG_PORTS="ports.conf"
-APACHE_CONFIG_FILENAME="apache2.conf"
-APACHE_CONFIG="/etc/apache2"
-APACHECTL="/usr/sbin/apache2ctl"
+ APACHE_CONFIG_PORTS="ports.conf"
+ APACHE_CONFIG_FILENAME="apache2.conf"
+ APACHE_CONFIG="/etc/apache2"
+ APACHECTL="/usr/sbin/apache2ctl"
 #
 # Set the virtual host configuration directory
-APACHE_VIRTUAL_HOSTS_ENABLED="sites-enabled"
-APACHE_VIRTUAL_HOSTS_AVAILABLE="sites-available"
+ APACHE_VIRTUAL_HOSTS_ENABLED="sites-enabled"
+ APACHE_VIRTUAL_HOSTS_AVAILABLE="sites-available"
 #
-# By default, use the site folders that get created will be 0wn3d by this group
-OWNER_GROUP="www-data"
+# By default, use the site folders that get created will be owned by this group
+ OWNER_GROUP="www-data"
 #
 # don't want to be nagged about "fixing" your DocumentRoot?  Set this to "yes".
-SKIP_DOCUMENT_ROOT_CHECK="yes"
+ SKIP_DOCUMENT_ROOT_CHECK="yes"
 #
 # If Apache works on a different port than the default 80, set it here
-APACHE_PORT="80"
+ APACHE_PORT="80"
 #
-#======= DO NOT EDIT BELOW THIS lINE UNLESS YOU KNOW WHAT YOU ARE DOING ========
+# Set the errorlog for the VirtualHost
+ ERROR_LOG="/var/log/apache2"
+
+# Set to yes, if you want the script to create an index.html file 
+# NB: If there's no index.html or index.php the script will add one 
+ CREATE_INDEX="no"
+
+# == DO NOT EDIT BELOW THIS lINE UNLESS YOU KNOW WHAT YOU ARE DOING ==
+# Ubuntu version dash script version. do not change!
+VERSION="12.04-1"
 
 if [ `whoami` != 'root' ]; then
     echo "You must be running with root privileges to run this script."
@@ -119,6 +146,9 @@ else
             VIRTUALHOST=$2
             DELETE=0
         fi
+    elif [ $1 = "--version" ]; then 
+        echo "Virtualhost.sh version: "$VERSION
+        exit 1
     else
         VIRTUALHOST=$1
     fi
@@ -129,7 +159,7 @@ fi
 #
 if [ ! -z $DELETE ]; then
     echo -n "- Deleting virtualhost, $VIRTUALHOST... Continue? [Y/n]: "
-
+    
     read continue
 
     case $continue in
@@ -162,7 +192,7 @@ if [ ! -z $DELETE ]; then
                         fi
                         ;;
                 esac
-
+            fi
                 echo -n "  - Deleting virtualhost file... ($APACHE_CONFIG/$APACHE_VIRTUAL_HOSTS_ENABLED/$VIRTUALHOST) and ($APACHE_CONFIG/$APACHE_VIRTUAL_HOSTS_AVAILABLE/$VIRTUALHOST) "
                 /usr/sbin/a2dissite $VIRTUALHOST 1>/dev/null 2>/dev/null
                 rm $APACHE_CONFIG/$APACHE_VIRTUAL_HOSTS_AVAILABLE/$VIRTUALHOST
@@ -172,7 +202,6 @@ if [ ! -z $DELETE ]; then
                 ## /usr/sbin/apachectl graceful 1>/dev/null 2>/dev/null
                 $APACHECTL graceful 1>/dev/null 2>/dev/null
                 echo "done"
-            fi
         fi
     else
         echo "- Virtualhost $VIRTUALHOST does not currently exist. Aborting..."
@@ -237,11 +266,12 @@ __EOT
     fi
 fi
 
-if ! grep -q -E "^NameVirtualHost *:$APACHE_PORT" $APACHE_CONFIG/$APACHE_CONFIG_PORTS ; then
 
-    echo "$APACHE_CONFIG_FILENAME not ready for virtual hosting. Fixing..."
-    cp $APACHE_CONFIG/$APACHE_CONFIG_FILENAME $APACHE_CONFIG/$APACHE_CONFIG_FILENAME.original
-    echo "NameVirtualHost $IP_ADDRESS:$APACHE_PORT" >> $APACHE_CONFIG/$APACHE_CONFIG_FILENAME
+if ! grep -q -E "^NameVirtualHost \*:$APACHE_PORT" $APACHE_CONFIG/$APACHE_CONFIG_PORTS ; then
+
+    echo "$APACHE_CONFIG_PORTS not ready for virtual hosting. Fixing..."
+    cp $APACHE_CONFIG/$APACHE_CONFIG_PORTS $APACHE_CONFIG/$APACHE_CONFIG_PORTS.original
+    echo "NameVirtualHost *:$APACHE_PORT" >> $APACHE_CONFIG/$APACHE_CONFIG_PORTS
 
     if [ ! -d $APACHE_CONFIG/$APACHE_VIRTUAL_HOSTS_AVAILABLE ]; then
         mkdir $APACHE_CONFIG/$APACHE_VIRTUAL_HOSTS_AVAILABLE
@@ -263,7 +293,10 @@ __EOT
         fi
     fi
 
-    echo "Include /$APACHE_CONFIG/$APACHE_VIRTUAL_HOSTS_ENABLED"  >> $APACHE_CONFIG/$APACHE_CONFIG_FILENAME
+    if ! grep -q -E "^Include $APACHE_VIRTUAL_HOSTS_ENABLED" $APACHE_CONFIG/$APACHE_CONFIG_FILENAME ; then
+        cp $APACHE_CONFIG/$APACHE_CONFIG_FILENAME $APACHE_CONFIG/$APACHE_CONFIG_FILENAME.original
+        echo "Include $APACHE_CONFIG/$APACHE_VIRTUAL_HOSTS_ENABLED"  >> $APACHE_CONFIG/$APACHE_CONFIG_FILENAME
+    fi
 fi
 
 
@@ -382,64 +415,66 @@ fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Create a default index.html if there isn't already one there
 #
-if [ ! -e $DOC_ROOT_PREFIX/$FOLDER/index.html -a ! -e $DOC_ROOT_PREFIX/$FOLDER/index.php ]; then
+if [ $CREATE_INDEX == 'yes']; then
+    if [ ! -e $DOC_ROOT_PREFIX/$FOLDER/index.html -a ! -e $DOC_ROOT_PREFIX/$FOLDER/index.php ]; then
 
-    cat << __EOF >$DOC_ROOT_PREFIX/$FOLDER/index.html
-    <html>
-    <head>
-    <title>Welcome to $VIRTUALHOST</title>
-    </head>
-    <style type="text/css">
-    body, div, td { font-family: "Lucida Grande"; font-size: 12px; color: #666666; }
-    b { color: #333333; }
-    .indent { margin-left: 10px; }
-    </style>
-    <body link="#993300" vlink="#771100" alink="#ff6600">
+        cat << __EOF >$DOC_ROOT_PREFIX/$FOLDER/index.html
+        <html>
+        <head>
+        <title>Welcome to $VIRTUALHOST</title>
+        </head>
+        <style type="text/css">
+        body, div, td { font-family: "Lucida Grande"; font-size: 12px; color: #666666; }
+        b { color: #333333; }
+        .indent { margin-left: 10px; }
+        </style>
+        <body link="#993300" vlink="#771100" alink="#ff6600">
 
-    <table border="0" width="100%" height="95%"><tr><td align="center" valign="middle">
-    <div style="width: 500px; background-color: #eeeeee; border: 1px dotted #cccccc; padding: 20px; padding-top: 15px;">
-    <div align="center" style="font-size: 14px; font-weight: bold;">
-    Congratulations!
-    </div>
+        <table border="0" width="100%" height="95%"><tr><td align="center" valign="middle">
+        <div style="width: 500px; background-color: #eeeeee; border: 1px dotted #cccccc; padding: 20px; padding-top: 15px;">
+        <div align="center" style="font-size: 14px; font-weight: bold;">
+        Congratulations!
+        </div>
 
-    <div align="left">
-    <p>If you are reading this in your web browser, then the only logical conclusion is that the <b><a href="http://$VIRTUALHOST/">http://$VIRTUALHOST/</a></b> virtualhost was setup correctly. :)</p>
+        <div align="left">
+        <p>If you are reading this in your web browser, then the only logical conclusion is that the <b><a href="http://$VIRTUALHOST/">http://$VIRTUALHOST/</a></b> virtualhost was setup correctly. :)</p>
 
-    <p>You can find the configuration file for this virtual host in:<br>
-    <table class="indent" border="0" cellspacing="3">
-    <tr>
-    <td><b>$APACHE_CONFIG/$APACHE_VIRTUAL_HOSTS_AVAILABLE/$VIRTUALHOST</b></td>
-    </tr>
-    </table>
-    </p>
-
-    <p>You will need to place all of your website files in:<br>
-    <table class="indent" border="0" cellspacing="3">
-    <tr>
-    <td><b><a href="file://$DOC_ROOT_PREFIX/$FOLDER">$DOC_ROOT_PREFIX/$FOLDER</b></a></td>
-    </tr>
-    </table>
-    </p>
-
-    <p>This script is based upon the excellent virtualhost (V1.04) script by Patrick Gibson <patrick@patrickg.com> for OS X. 
-    You can download the original script for OS X from Patrick's website: <b><a href="http://patrickg.com/virtualhost">http://patrickg.com/virtualhost</a></b>
-    </p>
-    <p>
-    For the latest version of this script for Ubuntu go to <b><a href="https://github.com/pgib/virtualhost.sh/tree/ubuntu">Github</a></b>!<br/>	
-        The Ubuntu Version is based on Bjorn Wijers script. Visit Bjorn Wijers' website: <br />
-        <b><a href="http://burobjorn.nl">http://burobjorn.nl</a></b><br>
-
+        <p>You can find the configuration file for this virtual host in:<br>
+        <table class="indent" border="0" cellspacing="3">
+        <tr>
+        <td><b>$APACHE_CONFIG/$APACHE_VIRTUAL_HOSTS_AVAILABLE/$VIRTUALHOST</b></td>
+        </tr>
+        </table>
         </p>
-        </div>
 
-        </div>
-        </td></tr></table>
+        <p>You will need to place all of your website files in:<br>
+        <table class="indent" border="0" cellspacing="3">
+        <tr>
+        <td><b><a href="file://$DOC_ROOT_PREFIX/$FOLDER">$DOC_ROOT_PREFIX/$FOLDER</b></a></td>
+        </tr>
+        </table>
+        </p>
 
-        </body>
-        </html>
+        <p>This script is based upon the excellent virtualhost (V1.04) script by Patrick Gibson <patrick@patrickg.com> for OS X. 
+        You can download the original script for OS X from Patrick's website: <b><a href="http://patrickg.com/virtualhost">http://patrickg.com/virtualhost</a></b>
+        </p>
+        <p>
+        For the latest version of this script for Ubuntu go to <b><a href="https://github.com/pgib/virtualhost.sh/tree/ubuntu">Github</a></b>!<br/>	
+            The Ubuntu Version is based on Bjorn Wijers script. Visit Bjorn Wijers' website: <br />
+            <b><a href="http://burobjorn.nl">http://burobjorn.nl</a></b><br>
+
+            </p>
+            </div>
+
+            </div>
+            </td></tr></table>
+
+            </body>
+            </html>
 __EOF
-        chown $USER:$OWNER_GROUP $DOC_ROOT_PREFIX/$FOLDER/index.html
+            chown $USER:$OWNER_GROUP $DOC_ROOT_PREFIX/$FOLDER/index.html
 
+        fi
     fi
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -448,15 +483,16 @@ __EOF
     echo -n "+ Creating virtualhost file... "
     cat << __EOF >$APACHE_CONFIG/$APACHE_VIRTUAL_HOSTS_AVAILABLE/$VIRTUALHOST
     <VirtualHost *:$APACHE_PORT>
-    DocumentRoot $DOC_ROOT_PREFIX/$FOLDER
-    ServerName $VIRTUALHOST
+      DocumentRoot $DOC_ROOT_PREFIX/$FOLDER
+      ServerName $VIRTUALHOST
+      ErrorLog $ERROR_LOG/$VIRTUALHOST-error.log
 
-    ScriptAlias /cgi-bin $DOC_ROOT_PREFIX/$FOLDER/cgi-bin
+      ScriptAlias /cgi-bin $DOC_ROOT_PREFIX/$FOLDER/cgi-bin
 
-    <Directory $DOC_ROOT_PREFIX/$FOLDER>
-    Options All
-    AllowOverride All
-    </Directory>
+      <Directory $DOC_ROOT_PREFIX/$FOLDER>
+        Options All
+        AllowOverride All
+      </Directory>
     </VirtualHost>
 __EOF
 
