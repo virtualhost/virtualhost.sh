bf96223779970365423bd53c731d859e9ed20731
Scott Reeves <cottser@mailworks.org>
2014-10-16 19:00:28 -0400

    Revert "Die if we can't make our document root."
    This reverts commit f3c4a5547603337d049191716084d6f300cc7c0c.

--- virtualhost.sh
+++ virtualhost.sh
@@ -815,7 +815,7 @@ fi
 # Create the folder if we need to...
 if [ ! -d "${FOLDER}" ]; then
   /bin/echo -n "  + Creating folder ${FOLDER}... "
-  su $USER -c "mkdir -p $FOLDER" || echo "  # Fatal: could not create ${FOLDER}"; exit 1
+  su $USER -c "mkdir -p $FOLDER"
   /bin/echo "done"
 fi
 
